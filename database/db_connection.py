748aeea3231efcc5d415795bf6f54b196db18e37
jieeeee151 <wongzhijie3@gmail.com>
2026-04-08 15:02:52 +0800

    update

--- database/db_connection.py
+++ database/db_connection.py
@@ -2,9 +2,16 @@ import os
 import mysql.connector
 
 def get_db_connection():
-    return mysql.connector.connect(
+    conn = mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE")
     )
+
+    # 🔥 SET MALAYSIA TIMEZONE (+8)
+    cursor = conn.cursor()
+    cursor.execute("SET time_zone = '+08:00'")
+    cursor.close()
+
+    return conn
